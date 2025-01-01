0d3f1c6a550f74ea66179e19172bcc5b48722880
Gabriel Staworko <staworkogabriel@gmail.com>
2025-01-01 18:30:09 +0100

    Update AboutMe.tsx

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -14,10 +14,9 @@ export default function Contact() {
 			<div className=" flex flex-col justify-center items-center gap-10 max-w-7xl w-[80vw]">
 				<h1 className="text-5xl">About Me</h1>
 				<p className="text-xl flex">
-					I started my coding journey because I was curious about how the web
-					works. Now, after countless hours of learning, I am looking for my
-					first job as a web developer. My current stack includes: React,
-					TypeScript, Next.js, Tailwind CSS, Node.js, and Express.js. I love to
+					My current stack includes: React, TypeScript, Next.js, 
+					Tailwind CSS, Node.js, and Express.js.
+					I know basics of Java, Python and C++, I love to
 					learn new technologies, and I am looking for new opportunities to
 					grow. I am an open-minded, optimistic person, and I love working with
 					people.
